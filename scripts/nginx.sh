524052f1c41d07658fa215cdaa5882873a133b95
phoenix <naman.j@consultadd.com>
2024-03-19 11:43:36 -0400

    Initial commit.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/dekhoghar/nginx/nginx.conf /etc/nginx/sites-available/GharDekho
-sudo ln -s /etc/nginx/sites-available/GharDekho /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/dekhoghar/nginx/nginx.conf /etc/nginx/sites-available/default
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
